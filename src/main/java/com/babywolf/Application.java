6ab3567e68b9bc4ff1084388996c09d56b3c17cc
babywolf <591138406@qq.com>
2017-05-15 21:12:40 +0800

    commit

---
+++ src/main/java/com/babywolf/Application.java
@@ -0,0 +1,14 @@
+package com.babywolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by babywolf on 17/5/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
