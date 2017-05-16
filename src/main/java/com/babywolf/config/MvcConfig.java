cc3e010d6b2a02efaa6ef0a48c23cf9b471705a9
babywolf <591138406@qq.com>
2017-05-16 10:56:20 +0800

    commit

---
+++ src/main/java/com/babywolf/config/MvcConfig.java
@@ -0,0 +1,12 @@
+package com.babywolf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by babywolf on 17/5/16.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+}
