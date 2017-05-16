8ba61bbec2b6d6d8a903b97408db26320ac23e93
babywolf <591138406@qq.com>
2017-05-16 17:33:38 +0800

    commit

---
+++ src/main/java/com/babywolf/authorization/resolvers/CurrentUserMethodArgumentResolver.java
@@ -0,0 +1,42 @@
+package com.babywolf.authorization.resolvers;
+
+import com.babywolf.authorization.annotation.CurrentUser;
+import com.babywolf.config.Constants;
+import com.babywolf.domain.User;
+import com.babywolf.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+
+/**
+ * Created by babywolf on 17/5/16.
+ */
+public class CurrentUserMethodArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        if (methodParameter.getParameterType().isAssignableFrom(User.class) &&
+                methodParameter.hasParameterAnnotation(CurrentUser.class)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        Long currentUserId = (Long) nativeWebRequest.getAttribute(Constants.CURRENT_USER_ID, RequestAttributes.SCOPE_REQUEST);
+        if (currentUserId != null) {
+            //从数据库中查询并返回
+            return userRepository.findOne(currentUserId);
+        }
+        throw new MissingServletRequestPartException(Constants.CURRENT_USER_ID);
+    }
+}
