79bae15f33de105aaa913384fe657904fc581459
babywolf <591138406@qq.com>
2017-05-16 13:48:45 +0800

    commit

---
+++ src/main/java/com/babywolf/controller/TokenController.java
@@ -0,0 +1,12 @@
+package com.babywolf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by babywolf on 17/5/16.
+ */
+@RestController
+@RequestMapping(value = "/tokens")
+public class TokenController {
+}
